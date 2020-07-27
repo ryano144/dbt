ca8e9539955aeb2f492e8497511a5197c2c74126
Ryan Gross <ryangross@RGROSS-MBP-CHI.local>
2020-07-27 11:39:50 -0400

    Updated the snowflake connector version per #2613

--- plugins/snowflake/setup.py
+++ plugins/snowflake/setup.py
@@ -40,7 +40,7 @@ setup(
     },
     install_requires=[
         'dbt-core=={}'.format(package_version),
-        'snowflake-connector-python==2.2.1',
+        'snowflake-connector-python==2.2.3',
         'azure-common<2.0.0',
         'azure-storage-blob<12.0.0',
         'urllib3>=1.20,<1.26.0',
